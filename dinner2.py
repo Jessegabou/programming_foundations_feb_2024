d4aaa6bfb64736eb47596b6c110d81098e858e23
molade123 <molade0924@gmail.com>
2024-04-05 18:21:29 -0400

    final project

---
+++ dinner2.py
@@ -0,0 +1,19 @@
+import csv
+
+with open('dinner.csv', newline='') as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader)
+
+    for row in csvreader:
+        print(row)
+        # process
+        dinner_decisions = f"""
+        Hi, my name is {row[0]}.
+        I chose {row[1]} as my main meal!
+        To go with it, I chose {row[2]} and {row[3]} as my sides.
+        And the best part, I have {row[4]}, {row[5]}, and {row[6]} waiting for me dessert!
+        Let's eat!
+        """
+
+        #output
+        print(dinner_decisions)
